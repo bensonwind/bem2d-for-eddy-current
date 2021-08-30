2de4ffeba33fdfe3f08e205feb7432fba286dd02
bensonwind <33997937+bensonwind@users.noreply.github.com>
2021-08-30 15:24:47 +0800

    Update excitationdef1.m

--- excitationdef1.m
+++ excitationdef1.m
@@ -2,15 +2,13 @@ function [F0]=excitationdef1(k,pxy,xq,yq)
 
 % [G0dir,G0ref,dG0dirdR1,dG0refdR2,Pbeta,dPbetadx,dPbetady]=greendef(k,pxyb,betaP,xq,yq);
 %
-% Calculates the 2D Green's function for free field, rigid plane or
-% plane with impedance.
+% Calculates the current excitation source from Gauss's divergence theorem.
 %
 % Note:
 %   To obtain the incident pressure on the nodes from a line source in the absence
 %   of the body using this function, do like this:
 %
-%    [G0dir,G0ref,dG0dirdR1,dG0refdR2,Pbeta,dPbetadx,dPbetady]=greendef(k,position,betaP,xyb(:,1),xyb(:,2));
-%    inc_pressure=i/4*(G0dir+G0ref)+Pbeta;
+%    [F0]=excitationdef1(k,position,xyb(:,1),xyb(:,2));
 %
 %   where 'position' are the [x,y] coordinates of the line source and xyb is the node
 %   positions geometry file. 
@@ -26,12 +24,7 @@ function [F0]=excitationdef1(k,pxy,xq,yq)
 %             for each point to calculate (e.g. integration points). 
 %
 %  Output variables:
-%   -G0dir:    direct Green's function for free-field or rigid plane.
-%   -G0ref:    reflected Green's function for free-field or rigid plane.
-%   -dG0dirdR1:derivative of the direct Green's function.
-%   -dG0dirdR2:derivative of the reflected Green's function.
-%   -Pbeta:   correction term for a plane with finite impedance.
-%   -dPbetadx,dPbetady :derivatives of the correction term.
+%   -F0:    direct Green's function for free-field or rigid plane.
 %
 % Reference:  -S.N. Chandler-Wilde and D.C. Hothersall: "Efficient calculation
 %             of the Green function for acoustic propagation above an homogeneous
